3af5c17f037b38673742d811ef28d1474dd69870
Grsz <hrgergsz@gmail.com>
2025-01-10 20:05:58 +0000

    feat: set up ts, eslint, prettier, husky, turbo, pnpm

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,81 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    ...['@vercel/style-guide/eslint/typescript'].map(require.resolve),
+    './javascript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project,
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
+    '@typescript-eslint/no-floating-promises': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.js?(x)', '*.mjs', '*.ts?(x)'],
+      rules: {
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'typeLike',
+            format: ['PascalCase'],
+          },
+          {
+            selector: 'enumMember',
+            format: ['camelCase'],
+          },
+        ],
+      },
+    },
+    {
+      files: ['*.test.js?(x)', '*test.ts?(x)'],
+      rules: {
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/no-confusing-void-expression': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+      },
+    },
+    {
+      files: ['*.?(c|m)js', '*.js?(x)'],
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+    },
+    {
+      files: ['*.json', '*.json5', '*.jsonc'],
+      parser: 'jsonc-eslint-parser',
+    },
+    {
+      files: ['package.json'],
+      parser: 'jsonc-eslint-parser',
+      rules: {
+        'jsonc/sort-keys': [
+          'error',
+          {
+            pathPattern: '^.+$',
+            order: { type: 'asc' },
+          },
+        ],
+      },
+    },
+  ],
+};
