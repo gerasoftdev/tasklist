f1a0db3613ef1ae10e337dbcef5ccf09156d1819
Grsz <hrgergsz@gmail.com>
2025-01-28 19:26:25 +0000

    feat: set up theme with variables, added Inter font

---
+++ packages/ui/src/theme.ts
@@ -0,0 +1,28 @@
+export const space = {
+  n: '0',
+  xxs: '0.125rem',
+  xs: '0.25rem',
+  sm: '0.5rem',
+  md: '1rem',
+  lg: '1.5rem',
+  xl: '2rem',
+  xxl: '2.5rem',
+};
+export const size = {
+  xxs: '1rem',
+  xs: '1.5rem',
+  sm: '3rem',
+  md: '5rem',
+  lg: '10rem',
+  xl: '15rem',
+  xxl: '25rem',
+};
+export const fontSize = {
+  xxs: '0.625rem',
+  xs: '0.875rem',
+  sm: '1rem',
+  md: '1.25rem',
+  lg: '1.5rem',
+  xl: '2rem',
+  xxl: '2.5rem',
+};
