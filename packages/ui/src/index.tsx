74ca36df3fd07e6cb1d507a3fee6263a2b468d79
Grsz <hrgergsz@gmail.com>
2025-01-28 19:43:42 +0000

    chore: export components

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -3,4 +3,7 @@ export * from './components/Row';
 export * from './components/Button';
 export * from './components/Screen';
 export * from './components/typography';
+export * from './components/Input';
+export * from './components/Checkbox';
+export * from './components/Modal';
 export * from './presets';
