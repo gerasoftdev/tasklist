2d42314e278eca83d2e38bb06ec1fee6c3d7e7fb
Grsz <hrgergsz@gmail.com>
2025-01-28 19:29:33 +0000

    feat: added typography, extended storybook with font, postcss

--- packages/ui/src/components/typography/Text/index.tsx
+++
@@ -1,5 +0,0 @@
-import type { FC, HTMLAttributes } from 'react';
-
-export const Text: FC<HTMLAttributes<HTMLSpanElement>> = (props) => (
-  <span {...props} />
-);
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,5 +2,5 @@ export * from './components/Col';
 export * from './components/Row';
 export * from './components/Button';
 export * from './components/Screen';
-export * from './components/typography/Text';
+export * from './components/typography';
 export * from './presets';
