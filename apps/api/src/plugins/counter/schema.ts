ef9587e5baee19d7cb0e2d3df96806a84be035e5
Grsz <hrgergsz@gmail.com>
2025-02-16 12:23:14 +0000

    chore: removed temporary counter logic

--- apps/api/src/plugins/counter/schema.ts
+++
@@ -1,7 +0,0 @@
-import type { increaseCounter } from '@/plugins/counter';
-
-declare module 'fastify' {
-  interface FastifyInstance {
-    increaseCounter: ReturnType<typeof increaseCounter>;
-  }
-}
--- apps/api/src/plugins/index.ts
+++ apps/api/src/plugins/index.ts
@@ -1,11 +1,5 @@
 import { configPlugin } from '@/plugins/config';
-import { counterPlugin } from '@/plugins/counter';
 import { authPlugin } from '@/plugins/auth';
 import { authServicePlugin } from '@/plugins/authService';
 
-export const plugins = [
-  configPlugin,
-  counterPlugin,
-  authPlugin,
-  authServicePlugin,
-];
+export const plugins = [configPlugin, authPlugin, authServicePlugin];
